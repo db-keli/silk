dfa9b8c92ea52b461709c1777c6fade02dad3134
kekeli <kekelidompeh@gmail.com>
2024-06-28 10:51:52 +0000

    feat: download video

--- ytd/ytd.py
+++ ytd/ytd.py
@@ -2,6 +2,7 @@ from pytube import Playlist
 from pytube import YouTube
 import io
 
+Buffer = io.BytesIO()
 def download_playlist(url, resolution):
     """
     Downloads all videos in a playlist from the given URL with the specified resolution.
@@ -16,10 +17,17 @@ def download_playlist(url, resolution):
     Raises:
         None
     """
-    p = Playlist(url)
-    for video in p.videos:
-        print(video.streams.get_by_resolution(resolution))
+    playlist = Playlist(url)
+    downloaded_buffers = []
+    
+    for video in playlist.videos:
+        stream = video.streams.filter(res=resolution).first()
+        stream.stream_to_buffer(Buffer)
+        Buffer.seek(0)
+        downloaded_buffers.append((video.title, Buffer))
 
+    print(downloaded_buffers)
+    
 def download_video(url, resolution):
     """
     Downloads a video from the given URL with the specified resolution.
@@ -31,22 +39,29 @@ def download_video(url, resolution):
     Returns:
         None
     """
+    print("hello")
     yt = YouTube(url)
-    yt.streams.get_by_resolution(resolution).download()
-    print("done")
+    print("me")
+    stream = yt.streams.filter(res=resolution).first()
+    if not stream:
+        print("none")
+        return
+    else:
+        print("there")
+        stream.stream_to_buffer(Buffer)
+        Buffer.seek(0)
+    print((yt.title, Buffer))
 
-# testing buffer
-yt = YouTube("https://youtu.be/aID6FoFhZqk?si=HohX-AtO1cEh3rHz")
-stream1=yt.streams[0]
+# # testing buffer
+# yt = YouTube("https://youtu.be/aID6FoFhZqk?si=HohX-AtO1cEh3rHz")
+# stream1=yt.streams[0]
 
-# creating buffer
-buffer = io.BytesIO()
-stream1.stream_to_buffer(buffer)
-buffer.seek(0)
-
-# Print buffer details
-print(f"Buffer size: {len(buffer.getvalue())} bytes")
+# # creating buffer
+# buffer = io.BytesIO()
+# stream1.stream_to_buffer(buffer)
+# buffer.seek(0)
+# # Print buffer details
+# print(f"Buffer size: {len(buffer.getvalue())} bytes")
 
 if __name__ == "main":
-
-    download_playlist("https://www.youtube.com/playlist?list=PLS1QulWo1RIaJECMeUT4LFwJ-ghgoSH6n","720p")
+    download_video("https://youtu.be/aID6FoFhZqk?si=HohX-AtO1cEh3rHz","360p")
