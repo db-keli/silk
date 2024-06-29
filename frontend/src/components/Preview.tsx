c86b24bf179612d6b5623041e36d2ec8bd8bcb3a
kekeli <kekelidompeh@gmail.com>
2024-06-29 17:29:39 +0000

    feat: preview on change

--- frontend/src/components/Preview.tsx
+++ frontend/src/components/Preview.tsx
@@ -1,16 +1,17 @@
-export default function Preview() {
+type Props = {
+  thumbnailLink: string;
+  title: string;
+};
+
+export default function Preview(prop: Props) {
   return (
     <div className="flex flex-row mt-5">
       <div className="basis-2/12" />
       <svg className="basis-3/12">
-        <image
-          href="https://i.ytimg.com/vi/CH50zuS8DD0/hq720.jpg"
-          height={"100%"}
-          width={"100%"}
-        />
+        <image href={prop.thumbnailLink} height={"100%"} width={"100%"} />
       </svg>
       <div className="basis-5/12 text-white ml-2">
-        <p className="text-lg">1 Minute Timer</p>
+        <p className="text-l">{prop.title}</p>
         <p>4:5</p>
       </div>
     </div>
