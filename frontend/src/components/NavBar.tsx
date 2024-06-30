d01137ac41f65a8896a62f420e669c39ec400bab
kekeli <kekelidompeh@gmail.com>
2024-06-30 13:05:03 +0000

    connect backend to frontend

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,24 +4,20 @@ export default function NavBar() {
       <nav className=" flex flex-row justify-center">
         <svg
           href="./logo.jpg"
-          className="basis-1/5 flex flex-row justify-center"
+          className=" max-sm:w-auto basis-1/5 flex flex-row justify-center"
           width="200"
           height="200"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <image
-            height="200"
-            width="200"
-            href="https://comicvine.gamespot.com/a/uploads/scale_small/11/111746/5120797-aristidefiloselle_c1033a1.jpg"
-          />
+          <image height="200" width="200" href="/logo.jpg" />
         </svg>
         <p className="text-white basis-3/5 text-center mt-11 text-5xl">
-          Grab the youtube videos
+          Aristides Silk
         </p>
         <div className="text-white basis-1/5 text-center">
           <ul>
-            <li>Playlists</li>
-            <li>Singles</li>
+            <li className="text-left">Playlists</li>
+            <li className="text-left">Singles</li>
           </ul>
         </div>
       </nav>
